46c8707d27b0f0e8a42bd584202f26dcb7b1ab60
mashiro~ <57851695+FuYon@users.noreply.github.com>
2022-04-04 12:59:51 +0800

    123

--- wp-login.php
+++ wp-login.php
@@ -2,17 +2,21 @@
 <html lang="en-US">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Log In &lsaquo; ChatLearning &#8212; WordPress</title>
+	<title>Log In &lsaquo; ❤ChatLearning-人形电脑天使心❤ &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="https://fuyon.github.io/123456/wp-includes/css/dashicons.min.css?ver=5.9.2" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://fuyon.github.io/123456/wp-includes/css/buttons.min.css?ver=5.9.2" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="https://fuyon.github.io/123456/wp-admin/css/forms.min.css?ver=5.9.2" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://fuyon.github.io/123456/wp-admin/css/l10n.min.css?ver=5.9.2" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="https://fuyon.github.io/123456/wp-admin/css/login.min.css?ver=5.9.2" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="http://chat-learning.skadi.top/wp-includes/css/dashicons.min.css?ver=5.9.2" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="http://chat-learning.skadi.top/wp-includes/css/buttons.min.css?ver=5.9.2" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="http://chat-learning.skadi.top/wp-admin/css/forms.min.css?ver=5.9.2" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="http://chat-learning.skadi.top/wp-admin/css/l10n.min.css?ver=5.9.2" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="http://chat-learning.skadi.top/wp-admin/css/login.min.css?ver=5.9.2" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
-		</head>
+	<link rel="icon" href="http://chat-learning.skadi.top/wp-content/uploads/2022/03/cropped-1分组-1-e1648203269141-1-32x32.png" sizes="32x32">
+<link rel="icon" href="http://chat-learning.skadi.top/wp-content/uploads/2022/03/cropped-1分组-1-e1648203269141-1-192x192.png" sizes="192x192">
+<link rel="apple-touch-icon" href="http://chat-learning.skadi.top/wp-content/uploads/2022/03/cropped-1分组-1-e1648203269141-1-180x180.png">
+<meta name="msapplication-TileImage" content="http://chat-learning.skadi.top/wp-content/uploads/2022/03/cropped-1分组-1-e1648203269141-1-270x270.png">
+	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
 	<script type="text/javascript">
 		document.body.className = document.body.className.replace('no-js','js');
@@ -20,7 +24,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="https://fuyon.github.io/123456/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="http://chat-learning.skadi.top/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -38,33 +42,33 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
-									<input type="hidden" name="redirect_to" value="https://fuyon.github.io/123456/wp-admin/">
+									<input type="hidden" name="redirect_to" value="http://chat-learning.skadi.top/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="https://fuyon.github.io/123456/wp-login.php?action=lostpassword">Lost your password?</a>
+								<a href="http://chat-learning.skadi.top/wp-login.php?action=lostpassword">Lost your password?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://fuyon.github.io/123456/">&larr; Go to ChatLearning</a>		</p>
+			<a href="http://chat-learning.skadi.top/">&larr; Go to ❤ChatLearning-人形电脑天使心❤</a>		</p>
 			</div>
-			<script type="text/javascript" src="https://fuyon.github.io/123456/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script type="text/javascript" src="https://fuyon.github.io/123456/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+			<script type="text/javascript" src="http://chat-learning.skadi.top/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="http://chat-learning.skadi.top/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script type="text/javascript" id="zxcvbn-async-js-extra">
 /*  */
-var _zxcvbnSettings = {"src":"https:\/\/fuyon.github.io\/123456\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"http:\/\/chat-learning.skadi.top\/wp-includes\/js\/zxcvbn.min.js"};
 /*  */
 </script>
-<script type="text/javascript" async="async" src="https://fuyon.github.io/123456/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script type="text/javascript" src="https://fuyon.github.io/123456/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
-<script type="text/javascript" src="https://fuyon.github.io/123456/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script type="text/javascript" src="https://fuyon.github.io/123456/wp-includes/js/dist/hooks.min.js?ver=1e58c8c5a32b2e97491080c5b10dc71c" id="wp-hooks-js"></script>
-<script type="text/javascript" src="https://fuyon.github.io/123456/wp-includes/js/dist/i18n.min.js?ver=30fcecb428a0e8383d3776bcdd3a7834" id="wp-i18n-js"></script>
+<script type="text/javascript" async="async" src="http://chat-learning.skadi.top/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="http://chat-learning.skadi.top/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="http://chat-learning.skadi.top/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="http://chat-learning.skadi.top/wp-includes/js/dist/hooks.min.js?ver=1e58c8c5a32b2e97491080c5b10dc71c" id="wp-hooks-js"></script>
+<script type="text/javascript" src="http://chat-learning.skadi.top/wp-includes/js/dist/i18n.min.js?ver=30fcecb428a0e8383d3776bcdd3a7834" id="wp-i18n-js"></script>
 <script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -80,17 +84,17 @@ var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type="text/javascript" src="https://fuyon.github.io/123456/wp-admin/js/password-strength-meter.min.js?ver=5.9.2" id="password-strength-meter-js"></script>
-<script type="text/javascript" src="https://fuyon.github.io/123456/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
+<script type="text/javascript" src="http://chat-learning.skadi.top/wp-admin/js/password-strength-meter.min.js?ver=5.9.2" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="http://chat-learning.skadi.top/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
 <script type="text/javascript" id="wp-util-js-extra">
 /*  */
 var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 /*  */
 </script>
-<script type="text/javascript" src="https://fuyon.github.io/123456/wp-includes/js/wp-util.min.js?ver=5.9.2" id="wp-util-js"></script>
+<script type="text/javascript" src="http://chat-learning.skadi.top/wp-includes/js/wp-util.min.js?ver=5.9.2" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"9f56a2c74a"};
+var userProfileL10n = {"user_id":"0","nonce":"40d2a43ae6"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
@@ -100,7 +104,7 @@ var userProfileL10n = {"user_id":"0","nonce":"9f56a2c74a"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type="text/javascript" src="https://fuyon.github.io/123456/wp-admin/js/user-profile.min.js?ver=5.9.2" id="user-profile-js"></script>
+<script type="text/javascript" src="http://chat-learning.skadi.top/wp-admin/js/user-profile.min.js?ver=5.9.2" id="user-profile-js"></script>
 	<script>
 	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
 	</script>
